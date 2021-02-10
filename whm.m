21048cc8d0a76e27c25f311f56c08438406ed689
zjquid <39973225+zjquid@users.noreply.github.com>
2021-02-09 23:39:50 -0600

    Add files via upload

---
+++ whm.m
@@ -0,0 +1,55 @@
+function whm()
+n = 30; % number of breaths
+t = 1.5; % length of breath
+
+hold(n,t);
+thold = 60; % length of breath hold
+retention(thold);
+
+hold(n,t);
+thold = 90;
+retention(thold);
+
+holt(n,t);
+thold = 90;
+retention(thold);
+
+end
+
+function hold(n,t)
+
+    for i = 1:n
+       disp(i);
+       disp("in");
+       pause(t);
+       disp("out");
+       disp("--------------------------------");
+       pause(t);
+    end
+
+end
+
+function retention(thold)
+
+disp("breathe in fully");
+pause(2);
+disp("let it out and hold breath");
+
+    for i = 1:thold
+        disp(i);
+        pause(1);
+    end
+
+disp("breathe in fully");
+disp("hold");
+pause(2);
+
+    for i = 1:15
+       disp(i);
+       pause(1);
+    end
+    
+disp("finish or prepare for next round");
+pause(5);
+
+end
